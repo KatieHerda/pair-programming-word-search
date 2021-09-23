685dd60ff45d544d7f783197509d83a66442a6cc
Katie <katie.herda2@gmail.com>
2021-09-23 01:03:47 +0000

    fixed intial code to pass tests. Added additional code to search for words vertically and to handle empty array input. Added additional tests in test file to test for these edge cases as well.

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,43 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+//Word search solver function, receives 2D array of letters and a word.
+//Function must check to find the word horiz and vertic andreturn true if the word is found.
 
-module.exports = wordSearch
+
+const wordSearch = (letters, word) => {
+
+  if (letters.length === 0) {
+    return false;
+  }
+
+  const horizontalJoin = letters.map(ls => ls.join(''));
+
+  for (const line of horizontalJoin) {
+    if (line.includes(word)) {
+      return true;
+    }
+  }
+
+  let newArr = [];
+  letters[0].forEach(() => newArr.push([]));
+  //loop through 2D array with nested loop
+  for (let r = 0; r < letters.length; r++) {
+    for (let c = 0; c < letters[r].length; c++) {
+      //identify each item in [0] of each row, push value to transposed array
+      newArr[c].push(letters[r][c]);
+    }
+        
+  }
+
+  const verticalJoin = newArr.map(ls => ls.join(''));
+
+  for (const line of verticalJoin) {
+    if (line.includes(word)) {
+      return true;
+    }
+  }
+    
+  return false;
+};
+
+
+module.exports = wordSearch;
+
